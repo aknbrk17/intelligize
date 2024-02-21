4bd85ce2df70f97e92f30915b1369cf717770666
Burak <akn.brk17@gmail.com>
2024-02-22 00:37:37 +0300

    first commit

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>ProfilPage</div>
+  )
+}
+
+export default ProfilePage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>Add</div>
+  )
+}
+
+export default AddTransformationTypePage
