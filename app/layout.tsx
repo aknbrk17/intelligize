89614edea51a6e4af8ba6536abe950a8035c96e1
Burak <akn.brk17@gmail.com>
2024-02-22 13:31:17 +0300

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider apperarance={{
+    <ClerkProvider appearance={{
       variables: { colorPrimary: '#624CF5'}
     }}>
       <html lang="en">
